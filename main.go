47a5df597cf06ab5962f2ad5ffff7708cfc9267e
shilangyu <xmarcinmarcin@gmail.com>
2019-11-30 16:42:45 +0100

    more informative error handling

--- main.go
+++ main.go
@@ -101,7 +101,7 @@ func main() {
 						cmd.Stderr = os.Stderr
 						err := cmd.Run()
 						if err != nil {
-							log.Fatalf("Failed to run the commands: %s", err)
+							log.Fatalf("Failed to run the commands \"%s\": %s", command, err)
 						}
 					}
 				}
